d3a94bafbac6e6c623546f7d93f1b4b9a6a8355d
Gyanendro Khomdram <khgyanendro77@gmail.com>
2017-11-01 19:39:39 +0530

    Added the Error Message View for Keyboard not properly set up.

--- app/src/main/java/com/gyanendrokh/meiteimayek/keyboard/activities/MainActivity.java
+++ app/src/main/java/com/gyanendrokh/meiteimayek/keyboard/activities/MainActivity.java
@@ -1,37 +1,68 @@
 package com.gyanendrokh.meiteimayek.keyboard.activities;
 
+import android.graphics.Color;
 import android.os.Bundle;
 import android.support.design.widget.NavigationView;
 import android.support.v4.view.GravityCompat;
 import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.CardView;
 import android.support.v7.widget.Toolbar;
+import android.text.Layout;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.RelativeLayout;
+import android.widget.TextView;
 
 import com.gyanendrokh.meiteimayek.keyboard.R;
+import com.gyanendrokh.meiteimayek.keyboard.views.HomeNotSetupView;
 
 public class MainActivity extends AppCompatActivity
   implements NavigationView.OnNavigationItemSelectedListener {
 
+  private Toolbar mToolbar;
   private DrawerLayout mDrawLayout;
+  private NavigationView mNavView;
+
+  private RelativeLayout mMainContent;
+  private CardView mCardNotSetUp;
 
   @Override
   protected void onCreate(Bundle savedInstanceState) {
     super.onCreate(savedInstanceState);
     setContentView(R.layout.activity_main);
 
-    Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
-    setSupportActionBar(toolbar);
+    mToolbar = (Toolbar) findViewById(R.id.toolbar);
+    setSupportActionBar(mToolbar);
 
     mDrawLayout = (DrawerLayout) findViewById(R.id.drawer_layout);
-    ActionBarDrawerToggle drawerToggle = new ActionBarDrawerToggle(this, mDrawLayout, toolbar,
-            R.string.navigation_drawer_open, R.string.navigation_drawer_close);
+    ActionBarDrawerToggle drawerToggle = new ActionBarDrawerToggle(this, mDrawLayout,
+      mToolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close);
     mDrawLayout.setDrawerListener(drawerToggle);
     drawerToggle.syncState();
 
-    NavigationView navigationView = (NavigationView) findViewById(R.id.nav_view);
-    navigationView.setNavigationItemSelectedListener(this);
+    mNavView = (NavigationView) findViewById(R.id.nav_view);
+    mNavView.setNavigationItemSelectedListener(this);
+
+    setBottomDevelopBy();
+    addCardNotSetUp();
+  }
+
+  private void addCardNotSetUp() {
+    mMainContent = (RelativeLayout) findViewById(R.id.main_content);
+    RelativeLayout.LayoutParams cardNotSetUpLParams = new RelativeLayout.LayoutParams(
+      RelativeLayout.LayoutParams.MATCH_PARENT,
+      RelativeLayout.LayoutParams.WRAP_CONTENT);
+    cardNotSetUpLParams.setMargins(5, 0, 5, 0);
+    mCardNotSetUp = HomeNotSetupView.getCardView(this);
+    mMainContent.addView(mCardNotSetUp, cardNotSetUpLParams);
+  }
+
+
+  private void removeCardNotSetUp() {
+    if(mCardNotSetUp == null) return;
+    mCardNotSetUp.setVisibility(View.GONE);
   }
 
   @Override
@@ -48,4 +79,12 @@ public class MainActivity extends AppCompatActivity
     mDrawLayout.closeDrawer(GravityCompat.START);
     return true;
   }
+
+  private void setBottomDevelopBy() {
+    TextView t1 = (TextView)findViewById(R.id.developed_w_by_1);
+    TextView t2 = (TextView)findViewById(R.id.developed_w_by_2);
+    t1.setTextColor(Color.BLACK);
+    t2.setTextColor(Color.BLACK);
+  }
+
 }
---
+++ app/src/main/java/com/gyanendrokh/meiteimayek/keyboard/views/HomeNotSetupView.java
@@ -0,0 +1,32 @@
+package com.gyanendrokh.meiteimayek.keyboard.views;
+
+import android.content.Context;
+import android.support.v7.widget.CardView;
+import android.view.View;
+import android.view.LayoutInflater;
+
+import com.gyanendrokh.meiteimayek.keyboard.R;
+
+public class HomeNotSetupView {
+
+  private Context mContext;
+  private CardView mCardView;
+  private LayoutInflater mInflater;
+
+  private HomeNotSetupView(Context context) {
+    mContext = context;
+    mInflater = (LayoutInflater) mContext.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+    getCardViewFromXML();
+  }
+
+  private void getCardViewFromXML() {
+    View v = mInflater.inflate(R.layout.home_not_setup, null);
+    mCardView = v.findViewById(R.id.card_not_setup);
+  }
+
+  public static CardView getCardView(Context con) {
+    HomeNotSetupView h = new HomeNotSetupView(con);
+    return h.mCardView;
+  }
+
+}
